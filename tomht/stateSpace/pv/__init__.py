f1531f6d5464a7fc58e31b15a1d42f89594239a5
Erik Liland <erik.liland@gmail.com>
2016-11-03 13:28:19 +0100

    Moved PV model to own module

---
+++ tomht/stateSpace/pv/__init__.py
@@ -0,0 +1,4 @@
+import os, sys
+sys.path.append(os.path.dirname(__file__))
+
+from .pv import *
---
+++ tomht/stateSpace/pv/pv.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def Phi(T):
+	return np.array([[1, 0, T, 0],
+					[0, 1, 0, T],
+					[0, 0, 1, 0],
+					[0, 0, 0, 1]])
+C 		= np.array([[1, 0, 0, 0],	#Also known as "H"
+					[0, 1, 0, 0]])
+H 		= C
+Gamma 	= np.diag([1,1],-2)[:,0:2]	#Disturbance matrix (only velocity)
+p 		= np.power(1e-2,1)			#Initial systen state variance
+P0 		= np.diag([p,p,p,p])		#Initial state covariance
+r		= np.power(1e-3,1)			#Measurement variance
+q 		= np.power(2e-2,1)			#Velocity variance variance
+R 		= np.eye(2) * r 			#Measurement/observation covariance
+def Q(T):
+	return np.eye(2) * q * T 		#Transition/system covariance (process noise)
