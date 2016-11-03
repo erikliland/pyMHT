8709a08de7f6315f4e7b238568177d186b8423bf
Erik Liland <erik.liland@gmail.com>
2016-11-03 13:27:42 +0100

    Adapted run files to new structure

--- examples/main.py
+++ examples/main.py
@@ -1,14 +1,13 @@
 import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__),".."))
 import matplotlib.pyplot as plt
 import numpy as np
 import pulp
-import cProfile
-import pstats
-sys.path.append(os.path.join(os.path.dirname(__file__),".."))
 import tomht
 from tomht.classDefinitions import Position
 import tomht.radarSimulator as sim
 import tomht.helpFunctions as hpf
+from tomht.stateSpace.pv import*
 
 seed = 5446
 nTargets = 4
@@ -16,65 +15,44 @@ p0 = Position(1,1)
 radarRange = 10.0 #meters
 maxSpeed = 2 #meters/second
 initialTargets = sim.generateInitialTargets(seed,nTargets,p0, radarRange, maxSpeed)
-initialTargets[3].state[3] *= -1
-initialTargets[3].state[2:4] *= 0.3
-initialTargets[1].state[2:4] *= 1.5
-initialTargets[1].state[2] *= -1.5
-initialTargets[1].state[2:4] *= 1.05
+initialTargets[3].state[3] 		*= -1
+initialTargets[3].state[2:4] 	*= 0.3
+initialTargets[1].state[2:4] 	*= 1.5
+initialTargets[1].state[2] 		*= -1.5
+initialTargets[1].state[2:4] 	*= 1.05
 print("Initial targets:")
 print(*initialTargets, sep='\n', end = "\n\n")
 
-nScans = 3
+nScans = 6
 timeStep = 1.0
-T 		= timeStep
-b 		= np.zeros(4) 				#Transition offsets
-C 		= np.array([[1, 0, 0, 0],	#Also known as "H"
-					[0, 1, 0, 0]])	
-d 		= np.zeros(2)				#Observation offsets
-Gamma 	= np.diag([1,1],-2)[:,0:2]	#Disturbance matrix (only velocity)
-P_d 	= 1.0						#Probability of detection
-p 		= np.power(1e-2,1)			#Initial systen state variance
-P0 		= np.diag([p,p,p,p])		#Initial state covariance
-r		= np.power(1e-3,1)			#Measurement variance
-q 		= np.power(2e-2,1)			#Velocity variance variance
-R 		= np.eye(2) * r 			#Measurement/observation covariance
-Q		= np.eye(2) * q * T 		#Transition/system covariance (process noise)
 lambda_phi 	= 2e-4					#Expected number of false measurements per unit 
 									# volume of the measurement space per scan
 lambda_nu 	= 0.0001				#Expected number of new targets per unit volume 
 									# of the measurement space per scan
-lambda_ex 	= lambda_phi+lambda_nu 	#Spatial density of the extraneous measurements
+P_d 		= 0.8					#Probability of detection
 sigma 		= 3						#Need to be changed to conficence
-sigma2		= np.power(sigma,2)
 N 		 	= 5						#Number of  timesteps to tail (N-scan)
-problemSizeLimit = 10000
 solver  	= pulp.CPLEX_CMD(None, 0,1,0,[],0.05)
 # solver  	= pulp.GLPK_CMD(None, 0,1,0,[])
 # solver  	= pulp.PULP_CBC_CMD()
-# solver  	= pulp.SYMPHONY()	#Unavailable
+# solver  	= pulp.SYMPHONY()		#Not implementet in PuLP yet
 # solver  	= pulp.GUROBI_CMD(None, 0,1,0,[])
-# solver  	= pulp.XPRESS()		#Unavailable
-def Phi(T):
-	from numpy import array
-	return np.array([[1, 0, T, 0],
-					[0, 1, 0, T],
-					[0, 0, 1, 0],
-					[0, 0, 0, 1]])
+# solver  	= pulp.XPRESS()			#Need licence
 
 
-simList = sim.simulateTargets(seed, initialTargets, nScans, timeStep, Phi(timeStep), Q, Gamma)
+simList = sim.simulateTargets(seed, initialTargets, nScans, timeStep, Phi(timeStep), Q(timeStep), Gamma)
 
 print("Sim list:")
 print(*simList, sep = "\n", end = "\n\n")
 
 scanList = sim.simulateScans(seed, simList, C, R, False, lambda_phi,radarRange, p0)
 
-tracker = tomht.Tracker(timeStep, Phi, C, Gamma, P_d, P0, R, Q, lambda_phi, lambda_nu, sigma, N, solver)
+tracker = tomht.Tracker(Phi, C, Gamma, P_d, P0, R, Q, lambda_phi, lambda_nu, sigma, N, solver)
 
 # print("Scan list:")
 # print(*scanList, sep = "\n", end = "\n\n")
 
-for initialTarget in initialTargets[0:3]:
+for initialTarget in initialTargets:
  	tracker.initiateTarget(initialTarget)
 
 for scanIndex, measurementList in enumerate(scanList):
@@ -100,5 +78,5 @@ hpf.plotActiveTrack(trackNodes)
 plt.axis("equal")
 plt.xlim((p0.x-radarRange*1.05, p0.x + radarRange*1.05))
 plt.ylim((p0.y-radarRange*1.05, p0.y + radarRange*1.05))
-plt.show()
+# plt.show()
 print("Done :)")
--- examples/run_dynamicAgents.py
+++ examples/run_dynamicAgents.py
@@ -1,23 +1,37 @@
-from classDefinitions import * 
-import helpFunctions as hpf
-import radarSimulator as sim
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__),".."))
 import tomht
-import os
+from tomht.classDefinitions import Position
+import tomht.radarSimulator as sim
+import tomht.helpFunctions as hpf
+from tomht.stateSpace.pv import *
+import pulp
 
 # file = './data/dynamic_agents_full_cooperation.txt'
-readfile = './data/dynamic_agents_partial_cooporation.txt'
+readfile = '../data/dynamic_agents_partial_cooporation.txt'
 (initialTargets, simList) = sim.importFromFile(readfile, startLine = 140*4)
 
 seed = 5446
+lambda_phi 	= 2e-4					#Expected number of false measurements per unit 
+									# volume of the measurement space per scan
+lambda_nu 	= 0.0001				#Expected number of new targets per unit volume 
+									# of the measurement space per scan
+P_d 		= 0.8					#Probability of detection
+sigma 		= 3						#Need to be changed to conficence
+N 		 	= 5						#Number of  timesteps to tail (N-scan)
+solver  	= pulp.CPLEX_CMD(None, 0,1,0,[],0.05)
 (p0, radarRange) = sim.findCenterPositionAndRange(simList)
-scanList = sim.simulateScans(seed, simList, tomht.C, tomht.R, False, 
-									tomht.lambda_phi,radarRange, p0, tomht.P_d)
+scanList = sim.simulateScans(seed, simList, C, R, False, lambda_phi,radarRange, p0, P_d)
+
+
+tracker = tomht.Tracker(Phi, C, Gamma, P_d, P0, R, Q, lambda_phi, lambda_nu, sigma, N, solver)
 
 for initialTarget in initialTargets:
- 	tomht.initiateTarget(initialTarget)
-associationHistory = []
+ 	tracker.initiateTarget(initialTarget)
 for scanIndex, measurementList in enumerate(scanList):
-	associationHistory.append(tomht.addMeasurementList(measurementList))
-savefile = os.path.splitext(readfile)[0] +"[Pd="+str(tomht.P_d)+",N="+str(tomht.N)+"]"+".txt"
-trackList = hpf.backtrackNodePositions(associationHistory[-1])
+	trackNodes = tracker.addMeasurementList(measurementList)
+	if scanIndex == 50:
+		break
+savefile = os.path.splitext(readfile)[0] +"[Pd="+str(P_d)+",N="+str(N)+"]"+".txt"
+trackList = hpf.backtrackNodePositions(trackNodes)
 hpf.writeTracksToFile(savefile, trackList)
