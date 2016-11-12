ec022016fe20400938be193a4e2eacd109cc0a7b
erikliland <erik.liland@gmail.com>
2016-11-12 11:44:45 +0100

    Fixed merge issues

--- tomht/stateSpace/pv/pv.py
+++ tomht/stateSpace/pv/pv.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np
 
 def Phi(T):
@@ -17,23 +16,3 @@ q 		= np.power(4e-2,1)			#Velocity variance variance
 R 		= np.eye(2) * r 			#Measurement/observation covariance
 def Q(T):
 	return np.eye(2) * q * T 		#Transition/system covariance (process noise)
-=======
-import numpy as np
-
-def Phi(T):
-	return np.array([[1, 0, T, 0],
-					[0, 1, 0, T],
-					[0, 0, 1, 0],
-					[0, 0, 0, 1]])
-C 		= np.array([[1, 0, 0, 0],	#Also known as "H"
-					[0, 1, 0, 0]])
-H 		= C
-Gamma 	= np.diag([1,1],-2)[:,0:2]	#Disturbance matrix (only velocity)
-p 		= np.power(1e-2,1)			#Initial systen state variance
-P0 		= np.diag([p,p,p,p])		#Initial state covariance
-r		= np.power(4e-2,1)			#Measurement variance
-q 		= np.power(4e-2,1)			#Velocity variance variance
-R 		= np.eye(2) * r 			#Measurement/observation covariance
-def Q(T):
-	return np.eye(2) * q * T 		#Transition/system covariance (process noise)
->>>>>>> bdc854266624b31dcf021d5ed28f1a279df23d82
