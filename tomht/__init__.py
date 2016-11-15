4a2c959359b6a578b700ea4f453345964e57322a
erikliland <erik.liland@gmail.com>
2016-11-15 12:54:10 +0100

    Unsuccessfull atempt at improve structure

--- tomht/__init__.py
+++ tomht/__init__.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
 import os, sys
 sys.path.append(os.path.dirname(__file__))
+#sys.path.insert(0,'')
 from .tracker import *
--- tomht/stateSpace/__init__.py
+++ tomht/stateSpace/__init__.py
@@ -1,2 +1,2 @@
-import os, sys
-sys.path.append(os.path.dirname(__file__))
+#import os, sys
+#sys.path.append(os.path.dirname(__file__))
