4534753b509ee887d131d1bca72a254609074821
Erik Liland <erik.liland@gmail.com>
2016-11-21 16:11:18 +0100

    Imporved processing of raw files, and removed stationary targets

--- examples/processRawData.py
+++ examples/processRawData.py
@@ -4,28 +4,38 @@ import sys
 import argparse
 import simSettings as sim
 
-def cropRawFile(filepath,**kwargs):
-	timeScale = kwargs.get("timeScale",1)
-	savefile = os.path.splitext(os.path.basename(filepath))[0]+"_cropped.txt"
-	savefilepath = os.path.join(os.path.dirname(filePath)+"_cropped", savefile)
-	print("Loading", filepath)
-	print("Savefile", savefile)
-	print("Savefilepath", savefilepath)
-	if not os.path.exists(os.path.dirname(savefilepath)):
-			os.makedirs(os.path.dirname(savefilepath))
-	fIn = open(filepath,'r')
-	fOut = open(savefilepath,'w')
+def cropRawFile(fileName,**kwargs):
+	rawFileName 		= sim.generateRawFileName(fileName)
+	loadRawFilePath 	= sim.generateRawFilePath(fileName,rawFileName) 
+	saveFileName 		= fileName
+	saveFilePath 		= sim.generateFilePath(saveFileName)
+	saveCroppedFileName = sim.generateCroppedFileName(fileName) 
+	saveCroppedFilePath = sim.generateCroppedFilePath(saveCroppedFileName)	
+	print("Loading", rawFileName, 	  "  \t from \t", loadRawFilePath)
+	print("Saving", saveFileName, 	"\t\t\t to   \t",saveFilePath)
+	print("Saving", saveCroppedFileName,"\t t0   \t",saveCroppedFilePath)
+	
+	if not os.path.exists(os.path.dirname(saveFilePath)):
+			os.makedirs(os.path.dirname(saveFilePath))
+	if not os.path.exists(os.path.dirname(saveCroppedFilePath)):
+			os.makedirs(os.path.dirname(saveCroppedFilePath))
+	fIn = open(loadRawFilePath,'r')
+	fOut = open(saveFilePath,'w')
+	fOutCropped = open(saveCroppedFilePath,'w')
 	for line in fIn:
 		lineTime = float(line.split(',')[0])
+		targets = line.strip().split(',')[1:]
+		targets = targets[0:2*kwargs.get("maxTargets", len(targets)/2)]
+		fOut.write( str( lineTime/kwargs.get("timeScale",1)) +"," + ",".join(targets)+"\n" )
 		if lineTime >= kwargs.get("newStart",0):
 			if lineTime <= kwargs.get("newEnd",float('inf')):
-				fOut.write(str((lineTime-kwargs.get("newStart",0))/timeScale) +"," + ",".join(line.split(',')[1:]))
+				fOutCropped.write( str((lineTime-kwargs.get("newStart",0))/kwargs.get("timeScale",1)) +"," + ",".join(targets)+"\n" )
 	fIn.close()
 	fOut.close()
+	fOutCropped.close()
 
 
 if __name__ == '__main__':
 	os.chdir(os.path.dirname(os.path.abspath(__file__)))
 	for file in sim.files:
-		filePath = os.path.join(sim.loadLocation,os.path.splitext(file)[0],file)
-		cropRawFile(filePath, newStart = 100, timeScale = 2, newEnd = 200)
+		cropRawFile(file, newStart = 100, timeScale = 2, newEnd = 200, maxTargets = 5)
--- examples/simSettings.py
+++ examples/simSettings.py
@@ -6,7 +6,8 @@ files 		= [	'dynamic_agents_full_cooperation.txt',
 				'dynamic_and_static_agents_large_space.txt',
 				'dynamic_and_static_agents_narrow_space.txt'
 				]
-croppedFiles = [os.path.join(os.path.splitext(file)[0]+"_cropped.txt") for file in files]
+# rawFiles = [os.path.splitext(file)[0]+"-RAW.txt" for file in files]
+# croppedFiles = [os.path.join(os.path.splitext(file)[0]+"_cropped.txt") for file in files]
 PdList 		= [0.5, 0.7, 0.9]
 NList 		= [1, 3, 6]
 lambdaPhiList = [0, 1e-4, 2e-4, 4e-4]
@@ -17,7 +18,7 @@ lambda_nu 	= 0.0001#Expected number of new targets per unit volume
 eta2 		= 5.99 #chi2.ppf(confidence,2)
 threshold 	= 4 	#meter
 
-def generateFilePath(fileString, solver, P_d, N, lambda_phi):
+def generateResultFilePath(fileString, solver, P_d, N, lambda_phi):
 	return (
 		os.path.join(	loadLocation,
 						os.path.splitext(fileString)[0],
@@ -31,3 +32,19 @@ def generateFilePath(fileString, solver, P_d, N, lambda_phi):
 							+"]"
 							+".xml"
 			)
+
+
+def generateRawFileName(fileName):
+	return os.path.splitext(fileName)[0]+"-RAW.txt"
+
+def generateRawFilePath(fileName, rawFileName):
+	return os.path.join(loadLocation,os.path.splitext(fileName)[0],rawFileName)
+
+def generateFilePath(fileName):
+	return os.path.join(loadLocation,os.path.splitext(fileName)[0],fileName)
+
+def generateCroppedFileName(fileName):
+	return os.path.splitext(os.path.basename(fileName))[0]+"_cropped.txt"
+
+def generateCroppedFilePath(croppedFileName):
+	return os.path.join(loadLocation,os.path.splitext(croppedFileName)[0], croppedFileName)
