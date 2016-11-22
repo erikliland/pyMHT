51c3890b38a433e3d190b55aab7aa67c52b36dbc
erikliland <erik.liland@gmail.com>
2016-11-22 16:19:08 +0100

    Tuned tracker R and Q

--- tomht/stateSpace/pv/pv.py
+++ tomht/stateSpace/pv/pv.py
@@ -7,9 +7,9 @@ H 				= C
 Gamma 			= np.diag([1,1],-2)[:,0:2]	#Disturbance matrix (only velocity)
 p 				= np.power(1,2)			#Initial systen state variance
 P0 				= np.diag([p,p,p,p])		#Initial state covariance
-sigmaR_tracker 	= 2		#Measurement standard deviation used in kalman filter
+sigmaR_tracker 	= 0.8		#Measurement standard deviation used in kalman filter
 sigmaR_true		= 0.5	#Measurement standard deviation used in radar simulator (+- 1.25m)
-sigmaQ_tracker	= 1		#Target standard deviation used in kalman filter
+sigmaQ_tracker	= 0.5		#Target standard deviation used in kalman filter
 sigmaQ_true 	= 0.5	#Tardet standard deviation used in kalman filter
 #95% conficence = +- 2.5*sigma
 
