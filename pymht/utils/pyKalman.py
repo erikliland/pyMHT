c2df90c213440d2e119d41ce0e0e2632d7699da1
erikliland <erik.liland@gmail.com>
2017-02-25 12:39:45 +0100

    Added coveralls integration

--- pymht/utils/pyKalman.py
+++ pymht/utils/pyKalman.py
@@ -101,6 +101,7 @@ class KalmanFilter():
         self.S = None  # Residual covariance
         self.S_inv = None  # Inverse residual covariance
         self.K = None  # Kalman gain
+        self.predicted = False
         self.precalculated = False
 
     def predict(self, **kwargs):
@@ -116,9 +117,12 @@ class KalmanFilter():
         if not kwargs.get('local', False):
             self.x_bar = x_bar
             self.P_bar = P_bar
+            self.predicted = True
         return x_bar, P_bar
 
     def _precalculateMeasurementUpdate(self):
+        if not self.predicted:
+            self.predict()
         self.z_hat = self.C.dot(self.x_bar)
         self.S = self.C.dot(self.P_bar).dot(self.C.T) + self.R
         self.S_inv = np.linalg.inv(self.S)
@@ -158,7 +162,8 @@ class KalmanFilter():
             x_hat = self.x_bar
             P_hat = self.P_bar
         elif len(args) == 1:
-            x_hat = self.x_bar + self.K.dot(args[0])
+            y_tilde = args[0]
+            x_hat = self.x_bar + self.K.dot(y_tilde)
             P_hat = self.P_bar - self.K.dot(self.C).dot(self.P_bar)
         else:
             raise ValueError("Invalid number of arguments")
