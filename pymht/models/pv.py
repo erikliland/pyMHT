cae24b86bcb50af56d978a919447275c09015f13
Erik <erik.liland@gmail.com>
2017-03-09 11:36:50 +0100

    Added AIS variables

--- pymht/models/pv.py
+++ pymht/models/pv.py
@@ -12,11 +12,12 @@ C_AIS = np.eye(4, dtype=defaultType)
 Gamma = np.array(np.diag([1.0, 1.0], -2)[:, 0:2], dtype=defaultType)
 p = np.power(1.0, 2)  # Initial systen state variance
 P0 = np.array(np.diag([p, p, p, p]), dtype=defaultType)  # Initial state covariance
-sigmaR_tracker = 1.0  # Measurement standard deviation used in kalman filter
+sigmaR_RADAR_tracker = 1.0  # Measurement standard deviation used in kalman filterUnused
 # Measurement standard deviation used in radar simulator (+- 1.25m)
 sigmaR_true = 1.0
-sigmaQ_tracker = 1.0  # Target standard deviation used in kalman filter
-sigmaQ_true = 0.5  # Tardet standard deviation used in kalman filter
+sigmaR_AIS_tracker = 0.1
+sigmaQ_tracker = 1.0  # Target standard deviation used in kalman filterUnused
+sigmaQ_true = 0.5  # Tardet standard deviation used in kalman filterUnused
 # 95% conficence = +- 2.5*sigma
 GPS_COVARIANCE_PRECISE = np.copy(P0*0.5)
 
@@ -25,9 +26,11 @@ def Q(T, sigmaQ=sigmaQ_tracker):
     return np.array(np.eye(2) * np.power(sigmaQ, 2) * T, dtype=defaultType)
 
 
-def R(sigmaR=sigmaR_tracker):
+def R_RADAR(sigmaR=sigmaR_RADAR_tracker):
     return np.array(np.eye(2) * np.power(sigmaR, 2), dtype=defaultType)
 
+def R_AIS(sigmaR = sigmaR_AIS_tracker):
+    return np.array(np.eye(4) * np.power(sigmaR, 2), dtype=defaultType)
 
 def Phi(T):
     return np.array([[1.0, 0,    T,  0],
