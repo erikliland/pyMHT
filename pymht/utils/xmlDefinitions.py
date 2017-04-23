6d0ea69d8772935229e1edea19bcdf209b69b965
Erik <erik.liland@gmail.com>
2017-04-23 13:40:13 +0200

    Changed tags to match full state storage

--- pymht/utils/xmlDefinitions.py
+++ pymht/utils/xmlDefinitions.py
@@ -1,13 +1,16 @@
 simulationTag = "Simulation"
 trackTag = "Track"
-positionsTag = "Positions"
+statesTag = "States"
+stateTag = "State"
 positionTag = "Position"
+velocityTag = "Velocity"
 northTag = "North"
 eastTag = "East"
 mmsiTag = "mmsi"
 timeTag = "time"
 unsmoothedTag = "unsmoothed"
 smoothedTag = "smoothed"
+idTag = "ID"
 
 groundtruthAttrib = {'type':'groundTruth'}
 estimateAttrib = {'type':'estimate'}
