17157224f5041eec6e143d6671b6cc56689b8063
erikliland <erik.liland@gmail.com>
2017-06-16 14:38:17 +0200

    Updated setup, licence, requirements and readme

--- README.md
+++ README.md
@@ -1,9 +1,53 @@
 # pyMHT 
 
+## Track oriented, multi target, multi hypothesis tracker
+Multi frame multi target tracking module with 2/2&m/n initialization algorithm and an AIS aided track oriented multi hypothesis tracking algorithm.
+
+
+## Installation
+
+You can get the latest and greatest from
+[github](https://github.com/erikliland/pymht):
+
+    $ git clone git@github.com:erikliland/pymht.git pymht
+    $ cd pymht
+    $ sudo python setup.py install
+
+
+`pyMHT` depends on the following modules,
+
+* `Cython`    	(for compiling Munkres algorithm)
+* `numpy`     	(for core functionality)
+* `scipy`     	(for core functionality)
+* `matplotlib`	(for ploting)
+* `pytest`		(for testing)
+* `matplotlib`	(for ploting)
+* `Munkres` [[Github](https://github.com/jfrelinger/cython-munkres-wrapper)]
+*  `OR-TOOLS` (for solving ILP´s) [[Github](https://github.com/google/or-tools)]
+
+All modules except `OR-TOOLS` can be installed via pip:
+
+	$ pip install -r preRequirements.txt
+	$ pip install -r requirements.txt
+	
+`OR-TOOLS` must be installed manually.
+
+## Test instalation
+To test the instalation run in the pyMHT directory:
+
+		$ pytest
+This module does not contain any scenarios or examples. This is placed in another repository [pyMHT-simulator](https://github.com/erikliland/pyMHT-simulator). 
+
+## Background
+This Python module is the result of a project assignment and a Master´s thesis
+
+[Project report](https://mfr.osf.io/render?url=https://osf.io/2eeqd/?action=download%26mode=render)
+
+[Thesis]()
+
+## Build status
 Master [![Build Status](https://travis-ci.org/erikliland/pyMHT.svg?branch=master)](https://travis-ci.org/erikliland/pyMHT)
 
 Development [![Build Status](https://travis-ci.org/erikliland/pyMHT.svg?branch=development)](https://travis-ci.org/erikliland/pyMHT)
 
 Master [![Coverage Status](https://coveralls.io/repos/github/erikliland/pyMHT/badge.svg?branch=master)](https://coveralls.io/github/erikliland/pyMHT?branch=master)
-
-Track oriented, multi target, multi hypothesis tracker
--- setup.py
+++ setup.py
@@ -2,74 +2,33 @@
 Setup script for pyMHT by Erik Liland 2017
 """
 from setuptools import find_packages
+from pip.req import parse_requirements
 import os
 import sys
+from setuptools import setup
 
 if sys.version_info.major < 3:
     sys.exit('Sorry, Python 2 is not supported')
 
-if ('USE_CYTHON' in os.environ) and (int(os.environ['USE_CYTHON']) == 1):
-    print("Using Cython")
-    USE_CYTHON = True
-else:
-    print("NOT using Cython")
-    USE_CYTHON = False
-
-ext = '.pyx' if USE_CYTHON else '.c'
-
-if USE_CYTHON:
-    from distutils.extension import Extension
-    import numpy
-    extensions = [Extension("pymht.utils.cKalman", ["pymht/utils/cKalman" + ext]),
-                  Extension("pymht.utils.cFunctions", ["pymht/utils/cFunctions" + ext],
-                            include_dirs=[numpy.get_include()])]
-else:
-    extensions = None
-
 name = "pyMHT"
-version = "1.0"
+version = "2.0"
 author = "Erik Liland"
 author_email = "erik.liland@gmail.com"
 description = "A track oriented multi hypothesis tracker with integer linear programming"
 license = "BSD"
 keywords = 'mht tomht radar tracking track-split track split multi target multitarget'
 url = 'http://autosea.github.io/sf/2016/04/15/radar_ais/'
-install_requires = ['matplotlib', 'numpy', 'scipy', 'psutil', 'termcolor','Cython']
-
+install_requires = ['matplotlib', 'numpy', 'scipy', 'psutil', 'termcolor', 'Cython']
 packages = find_packages(exclude=['examples', 'docs'])
-print("Packages", packages)
 
-if USE_CYTHON:
-    from distutils.core import setup
-    from Cython.Build import cythonize
-    print("Cythonize extensions")
-    extensions = cythonize(extensions)
-    print("Setup with external modules")
-    setup(
-        name=name,
-        version=version,
-        author=author,
-        author_email=author_email,
-        description=description,
-        license=license,
-        keywords=keywords,
-        url=url,
-        packages=packages,
-        # include_package_data = True,
-        # install_requires = install_requires,
-        ext_modules=extensions,
-    )
-else:
-    from setuptools import setup
-    setup(
-        name=name,
-        version=version,
-        author=author,
-        author_email=author_email,
-        description=description,
-        license=license,
-        keywords=keywords,
-        packages=packages,
-        # include_package_data=True,
-        # install_requires=install_requires,
-    )
+setup(
+    name=name,
+    version=version,
+    author=author,
+    author_email=author_email,
+    description=description,
+    license=license,
+    keywords=keywords,
+    packages=packages,
+    install_requires=reqs
+)
